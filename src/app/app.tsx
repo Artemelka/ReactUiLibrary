75cb01a94852ed855e85255c3876861d8a4c94bd
artemelka <artemelka.ru@gmail.com>
2019-03-01 02:11:57 +0300

    connection Router in app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router'
 
 import './app.less';
 
-export interface AppPropsType { title: Array<string>; }
+
+interface ActionType {
+    type: string;
+}
+interface CounterType {
+    value: number;
+}
+interface AppPropsType {
+    counter: CounterType;
+    routing: any;
+    dispatch: (action: ActionType) => void;
+}
+interface mapStateType {
+    counter: CounterType;
+}
 
 const TEST_URL = '/api/getUsername';
-const requestTestServer = (url: string, queryParams?: string): any => {
+const TITLE = ['hello', 'world'];
+const requestTestServer = (url: string, queryParams?: string): void => {
     const finalUrl = queryParams ? `${url}?${queryParams}` : url;
 
     axios.get(finalUrl)
@@ -17,21 +34,49 @@ const requestTestServer = (url: string, queryParams?: string): any => {
         }).catch(requestError => console.log(requestError));
 };
 
-export class App extends React.Component<AppPropsType, {}> {
+@(connect(({counter}: mapStateType, routing) => ({counter, routing})) as any)
+class Index extends React.Component<AppPropsType, {}> {
+    handleClick = (): void => requestTestServer(TEST_URL);
+
+    handleCounterClick = (dir: boolean) => () => this.props.dispatch({type: dir ? 'INCREMENT' : 'DECREMENT'});
+
+    handleLinkClick = () => this.props.routing.history.push('/app?filter=123');
+
+    render() {
+        const { counter, routing } = this.props;
+        console.log('routing', routing);
+        return (
+            <React.Fragment>
+                {TITLE.map((item, index) => <h1 key={index}>{item}</h1>)}
+                <div>
+                    <button onClick={this.handleClick}>REQUEST</button>
+                </div>
+                <h2>counter : {counter.value}</h2>
+                <div>
+                    <button onClick={this.handleCounterClick(true)}>counter +</button>
+                    <button onClick={this.handleCounterClick(false)}>counter -</button>
+                </div>
+                <h3>{routing.history.location.pathname}</h3>
+                <h3>{routing.history.location.search}</h3>
+                <div>
+                    <button onClick={this.handleLinkClick}>link</button>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export class App extends React.Component<{}, {}> {
     componentDidMount(): void {
         requestTestServer(TEST_URL, 'name=tim');
     }
 
-    handleClick = () => requestTestServer(TEST_URL);
-
     render() {
-        const { title } = this.props;
-
         return (
             <div className='App'>
-                {title.map((item, index) => <h1 key={index}>{item}</h1>)}
-                <button onClick={this.handleClick}>REQUEST</button>
+                <Route path="/" component={Index} />
             </div>
         );
     }
 }
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,46 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { routerMiddleware, routerReducer } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
 import { App } from './app';
 import './index.less';
 
-const ROOT = document.getElementById('App');
+interface ActionType {
+    type: string;
+}
+
+const ELEMENT_NAME = 'App';
+const ROOT = document.getElementById(ELEMENT_NAME);
+const rootReducers = (state = {value: 0}, action: ActionType) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {value: state.value + 1};
+        case 'DECREMENT':
+            return {value: state.value - 1};
+        default:
+            return state;
+    }
+};
+const reducers = combineReducers({
+    counter: rootReducers,
+    routing: routerReducer
+});
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const composeMiddleware = composeWithDevTools(applyMiddleware(middleware));
+const store = createStore(reducers, composeMiddleware);
+
 
 ReactDOM.render(
-    <App title={['hello', 'world']} />,
+    <Provider store={store}>
+        <Router history={history}>
+            <App />
+        </Router>
+    </Provider>,
     ROOT
 );
