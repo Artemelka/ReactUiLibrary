cb8150e7feeeae4228b323641e7de6a545b94d05
artemelka <artemelka.ru@gmail.com>
2019-04-17 00:41:38 +0300

    refactoring Checkbox component

--- src/app/elements/inputs/checkbox/checkbox.tsx
+++ src/app/elements/inputs/checkbox/checkbox.tsx
@@ -1,45 +1,103 @@
-import React, { Component, createRef, RefObject } from 'react';
+import React, { Component, createRef, KeyboardEvent, RefObject } from 'react';
 import classNames from 'classnames';
+import { Icon } from '../..';
+import { keyCodes } from '../../../../services';
 import './checkbox.less';
+import './toggle.less';
 
 export interface CheckboxProps {
-    disabled?: boolean;
     checked?: boolean;
+    disabled?: boolean;
+    id: string;
     name: string;
     onChange?: () => void;
+    toggle?: boolean;
     withRef?: (ref: HTMLElement) => void;
 }
+
+const iconProps = {
+    fontSize: 18,
+    name: 'angle-down',
+    size: '2x',
+    stack: 'fas'
+};
+const {ENTER, SPACE} = keyCodes;
+const targetKeyCodes = [ENTER, SPACE];
 
 export class Checkbox extends Component<CheckboxProps> {
     static defaultProps = {
         checked: false,
+        onChange: () => {},
         withRef: (ref: HTMLElement) => ref
     };
 
     componentDidMount() {
-        this.props.withRef(this.checkbox.current);
+        this.props.withRef(this.label.current);
     }
 
-    checkbox: RefObject<HTMLInputElement> = createRef();
+    label: RefObject<HTMLLabelElement> = createRef();
+    input: RefObject<HTMLInputElement> = createRef();
+
+    handleChange = () => {
+        const { disabled, onChange } = this.props;
+
+        if (!disabled) {
+            onChange();
+        }
+    };
+
+    handleFocus = () => {
+        // this.input.current.blur();
+        this.label.current.focus();
+    };
+
+    handleKeyPress = (event: KeyboardEvent<HTMLElement>) => {
+        const {keyCode, which } = event;
+        const code = keyCode || which;
+
+        if (targetKeyCodes.includes(code)) {
+            this.handleChange();
+        }
+    };
 
     render() {
-        const { checked, disabled, name, onChange } = this.props;
+        const { checked, disabled, id, name, toggle } = this.props;
         const checkboxStyle = classNames('Checkbox', {
             'Checkbox--checked': checked,
             'Checkbox--disabled': disabled
         });
+        const toggleStyle = classNames('Toggle', {
+            'Toggle--checked': checked,
+            'Toggle--disabled': disabled
+        });
+        const inputClasses = toggle ? 'Toggle__input' : 'Checkbox__input';
+        const hasIcon = !toggle && checked;
 
         return (
-            <label className={checkboxStyle}>
+            <label
+                className={toggle ? toggleStyle : checkboxStyle}
+                onKeyPress={this.handleKeyPress}
+                tabIndex={disabled ? -1 : 0}
+                ref={this.label}
+            >
                 <input
                     checked={checked}
-                    className="Checkbox__input"
+                    className={classNames(inputClasses)}
                     disabled={disabled}
+                    id={id}
                     name={name}
-                    onChange={onChange}
-                    ref={this.checkbox}
+                    onChange={this.handleChange}
+                    onFocus={this.handleFocus}
+                    tabIndex={-1}
+                    ref={this.input}
                     type="checkbox"
+                    value={id}
                 />
+                {hasIcon &&
+                    <span className={classNames('Checkbox__icon')}>
+                        <Icon {...iconProps}/>
+                    </span>
+                }
             </label>
         );
     }
--- src/app/pages/sand-box/checkbox/checkbox-sand-box.tsx
+++ src/app/pages/sand-box/checkbox/checkbox-sand-box.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, createRef, Fragment } from 'react';
 import { SandBox } from '../sand-box';
 import { Checkbox } from '../../../elements/inputs';
 import { CheckboxProps } from '../../../elements/inputs/checkbox/checkbox';
@@ -15,50 +15,74 @@ const checkboxProps: Array<Props> = [
     {
         disabled: true,
         checked: true,
-        heading: 'checkbox disabled',
+        id: 'test1',
+        heading: 'Checkbox disabled',
         name: 'test1',
         onChange: logger('checkbox Click disabled')
     }, {
         onChange: logger('checkbox Click'),
-        heading: 'checkbox',
+        id: 'test2',
+        heading: 'Checkbox',
         name: 'test2',
         checked: false
     }, {
-        heading: 'checkbox without prop checked',
+        heading: 'Checkbox without prop checked',
+        id: 'test3',
         name: 'test3',
         onChange: logger('checkbox without prop checked Click')
     },
 ];
+const checkboxToggleProps: Array<Props> = checkboxProps.map((props) => ({
+    ...props,
+    heading: props.heading.replace('Checkbox', 'Checkbox.Toggle')
+}));
 
 class CheckboxExample extends Component<Props, State> {
     state = {
         checked: this.props.checked
     };
 
+    setFocus = () => {
+        this.checkbox.focus();
+    };
+
+    checkbox: HTMLInputElement;
+
     handleChange = () => {
         this.setState(({checked}) => ({checked: !checked}));
         this.props.onChange();
     };
 
+    handleRef = (ref: HTMLInputElement) => this.checkbox = ref;
+
     render() {
-        const { checked, heading, onChange, ...restProps} = this.props;
+        const { checked, disabled, heading, onChange, ...restProps} = this.props;
 
         return (
             <Fragment>
                 <h3>{heading}</h3>
                 <Checkbox
                     {...restProps}
+                    disabled={disabled}
                     onChange={this.handleChange}
                     checked={this.state.checked}
+                    withRef={this.handleRef}
                 />
+                <br />
+                <br />
+                {!disabled && <button onClick={this.setFocus}>focus</button>}
             </Fragment>
         );
     }
 }
 
-const sandBoxItems = checkboxProps.map((props: Props, index: number) =>
+const CheckboxItems = checkboxProps.map((props: Props, index: number) =>
     <CheckboxExample {...props} key={index}/>
 );
+const CheckboxToggleItems = checkboxToggleProps.map((props: Props, index: number) =>
+    <CheckboxExample {...props} key={index} toggle/>
+);
+const sandBoxItems = [...CheckboxItems, ...CheckboxToggleItems];
 
 export const CheckboxSandBox = () => (
     <SandBox
