a202e83f0829149af5fbc2edb23518b81789bacc
Artem Emelyanov <anemelyanov@vtb.ru>
2019-05-02 19:37:24 +0300

    create Input component

--- src/app/elements/inputs/input/Input.tsx
+++ src/app/elements/inputs/input/Input.tsx
@@ -1,16 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component, createRef, RefObject, SyntheticEvent} from 'react';
 import classNames from 'classnames/bind';
+import { Button, IconModule } from '../../index';
 
 const style = require('./Input.less');
 const cn = classNames.bind(style);
 
-export class Input extends Component {
+interface Props {
+    disabled?: boolean;
+    onChange?: (event: SyntheticEvent<HTMLInputElement>, value: string) => void;
+}
+interface State {
+    focused: boolean;
+    value: string;
+}
+export class Input extends Component<Props, State> {
+    static defaultProps = {
+        onChange: () => false
+    };
+
+    state = {
+        focused: false,
+        value: ''
+    };
+
+    handleBlur = () => this.setState({focused: false});
+
+    handleChange = (event: SyntheticEvent<HTMLInputElement>) => {
+        const { value } = event.currentTarget;
+        this.setState({value});
+        this.props.onChange(event, value);
+    };
+
+    handleClearClick = () => {
+        this.setState({value: ''});
+        this.input.current.focus();
+        this.props.onChange(null, '');
+    };
+
+    handleFocus = () => this.setState({focused: true});
+
+    input: RefObject<HTMLInputElement> = createRef();
+
     render() {
+        const { focused, value } = this.state;
+        const { disabled } = this.props;
         return (
-            <input
-                className={cn('Input')}
-                {...this.props}
-            />
+            <div
+                className={cn('Input', {'Input--focused': focused})}
+            >
+                <input
+                    className={cn('Input__element', {'Input__element--disabled': disabled})}
+                    disabled={disabled}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                    onFocus={this.handleFocus}
+                    value={value}
+                    ref={this.input}
+                />
+                {Boolean(value) &&
+                    <div className={cn('Input__clear-button')}>
+                        <Button.Icon
+                            disabled={disabled}
+                            iconName={IconModule.IconNames.BACKSPACE}
+                            onClick={this.handleClearClick}
+                        />
+                    </div>
+                }
+            </div>
         );
     }
 }
--- src/app/pages/sand-box/input/input-sand-box.tsx
+++ src/app/pages/sand-box/input/input-sand-box.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import { SandBox } from '../sand-box';
 import { Input } from '../../../elements';
 
 const inputProps = [
     {
         id: 'test1',
-        name: 'test1'
+        name: 'test1',
+        onChange: (event: SyntheticEvent<HTMLInputElement>, value: string) => {
+            console.log('input value', value);
+        }
     }, {
         id: 'test2',
         name: 'test2',
