71ad89bfdf6e34a75b856501d8d35df3e44e9be0
artemelka <artemelka.ru@gmail.com>
2019-05-20 23:41:23 +0300

    rewrite Footer component

--- src/app/layouts/footer/Footer.tsx
+++ src/app/layouts/footer/Footer.tsx
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import classNames from 'classnames/bind';
-import { Anchor } from '../../elements';
-import { translate } from '../../../services/translate';
+import { Anchor, Select } from '../../elements';
+import { changeLocale, translate } from '../../../services/translate';
+import { TranslateState } from '../../../services/translate/reducer';
+import { Dispatch } from 'redux';
 
 const style = require('./Footer.less');
 const cn = classNames.bind(style);
 
 export const PROJECT_LINK = 'https://github.com/Artemelka/ReactUiLibrary';
 const HOME_URL = '/';
+const SELECT_WIDTH = 70;
+const selectOptions = [
+    {
+        value: 'ru',
+        title: 'russian-language'
+    }, {
+        value: 'en',
+        title: 'english-language'
+    }
+];
 
 interface Props {
+    changeLocale?: (locale: string) => (dispatch: Dispatch) => void;
     history?: {push: (url: string) => void};
+    translateDictionary?: TranslateState;
 }
 
 @(withRouter as any)
+@(connect(
+    (state: {[key: string]: any}) => ({ translateDictionary: state.translateDictionary }),
+    { changeLocale }
+) as any)
 export class Footer extends Component<Props> {
     handleClick = () => this.props.history.push(HOME_URL);
+
+    handleLangChange = (value: string) => this.props.changeLocale(value);
 
     render() {
         return (
@@ -26,7 +47,18 @@ export class Footer extends Component<Props> {
                     <Anchor label={translate('go-to-home')} onClick={this.handleClick}/>
                 </div>
                 <div className={cn('Footer__main')}>
-                    {this.props.children}
+                    <div className={cn('Footer__content')}>
+                        {this.props.children}
+                    </div>
+                    <div className={cn('Footer__select')}>
+                        <Select
+                            listOpenTop
+                            onChange={this.handleLangChange}
+                            options={selectOptions}
+                            value={this.props.translateDictionary.locale}
+                            inputWidth={SELECT_WIDTH}
+                        />
+                    </div>
                 </div>
             </footer>
         );
--- src/app/pages/library/LibraryFooter.tsx
+++ src/app/pages/library/LibraryFooter.tsx
@@ -1,42 +1,15 @@
-import React, { Component, SyntheticEvent } from 'react';
-import { Dispatch } from 'redux';
-import { connect } from 'react-redux';
-import { Select } from '../../elements';
-import { changeLocale } from '../../../services/translate';
-import { TranslateState } from '../../../services/translate/reducer';
-
-const selectOptions = [
-    {
-        value: 'ru',
-        title: 'russian-language'
-    }, {
-        value: 'en',
-        title: 'english-language'
-    }
-];
+import React, { Component } from 'react';
 
 interface Props {
-    changeLocale?: (locale: string) => (dispatch: Dispatch) => void;
-    translateDictionary?: TranslateState;
+
 }
 
-@(connect(
-    (state: {[key: string]: any}) => ({ translateDictionary: state.translateDictionary }),
-    { changeLocale }
-) as any)
 export class LibraryFooter extends Component<Props> {
-    handleLangChange = (value: string) => this.props.changeLocale(value);
 
     render() {
         return (
-            <div style={{textAlign: 'right'}}>
-                <Select
-                    listOpenTop
-                    onChange={this.handleLangChange}
-                    options={selectOptions}
-                    value={this.props.translateDictionary.locale}
-                    inputWidth={70}
-                />
+            <div>
+                123456789
             </div>
         );
     }
