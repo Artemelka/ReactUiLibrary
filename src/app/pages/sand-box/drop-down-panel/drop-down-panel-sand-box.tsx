b244b4f96b2c152c2222de4f4f2630dc859a1a0e
artemelka <artemelka.ru@gmail.com>
2019-07-24 22:41:21 +0300

    rewrite dropDownPanel sandbox

--- src/app/pages/sand-box/drop-down-panel/drop-down-panel-sand-box.tsx
+++ src/app/pages/sand-box/drop-down-panel/drop-down-panel-sand-box.tsx
@@ -1,97 +1,25 @@
-import React, { Component } from 'react';
-import { SandBox } from '../sand-box';
-import { DropDownPanel, DropDownDetails, DropDownSummary, IconModule } from '../../../elements';
-import { logger } from '../utils';
-import { DropDownPanelProps } from '../../../elements/drop-down-panel/DropDownPanel';
+import React from 'react';
+import { SandboxLayout } from '../../../components';
+import { DropDownPanelExample } from './components/drop-down-panel-example';
+import { DropDownPanelView } from './components/drop-down-panel-view';
 
-const { PLUS, TRASH } = IconModule.IconNames;
-const dropDownPanelActionProps = [
-    {}, {
-        actionIcon: {
-            iconName: TRASH,
-            onClick: logger('click action')
-        }
-    }, {
-        openingByIcon: true
-    }, {
-        actionIcon: {
-            iconName: TRASH,
-            onClick: logger('click action')
-        },
-        openingByIcon: true
-    }
+const { Sandbox } = SandboxLayout;
+const dropDownPanelProps = [
+    ['actionIcon', 'object', 'showed icon on right side {iconName: string, onClick: function'],
+    ['darkColor', 'boolean', 'dark theme style'],
+    ['emptyStyle', 'boolean', 'without style view'],
+    ['onChange', 'function', 'callback on open click event'],
+    ['onlyBorder', 'boolean', 'only border style'],
+    ['opened', 'boolean', 'open state'],
+    ['openingByIcon', 'boolean', 'opened panel on icon click']
 ];
-const dropDownPanelProps: Array<DropDownPanelProps> = [
-    {
-        opened: false
-    }, {
-        onlyBorder: true,
-        opened: false
-    }, {
-        darkColor: true,
-        opened: false
-    }, {
-        emptyStyle: true,
-        opened: false
-    }
-];
-const Header = () => <h2 style={{margin: 0}}>Heading</h2>;
-const Content = ({index}: {index: number}) => <div style={{height: `${index + 1}00px`}}>Content</div>;
-
-interface State {
-    opened: boolean;
-}
-interface Props extends DropDownPanelProps {
-    index: number;
-}
-
-class DropDownPanelExample extends Component<Props, State> {
-    static getDerivedStateFromProps (prevState: State, nextProps: DropDownPanelProps) {
-        return prevState.opened !== nextProps.opened ? {opened: nextProps.opened} : null;
-    }
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            opened: props.opened
-        };
-    }
-
-    handleChange = () => this.setState(({opened}) => ({opened: !opened}));
-
-    render() {
-        const { onChange, opened, index, ...restProps} = this.props;
-
-        return (
-            <DropDownPanel
-                {...restProps}
-                onChange={this.handleChange}
-                opened={this.state.opened}
-            >
-                <DropDownSummary>
-                    <Header/>
-                </DropDownSummary>
-                <DropDownDetails>
-                    <Content index={index}/>
-                </DropDownDetails>
-            </DropDownPanel>
-        );
-    }
-}
-
-const sandBoxItems = dropDownPanelProps.map((props, index) => () =>
-    dropDownPanelActionProps.map((actions, otherIndex) => (
-        <div style={{padding: '10px 0'}} key={Number(`${otherIndex}${index}`)}>
-            <DropDownPanelExample
-                {...props}
-                {...actions}
-                index={index}
-            />
-        </div>
-)));
 
 export const DropDownPanelSandBox = () => (
-    <SandBox
-        items={sandBoxItems}
+    <Sandbox
+        acceptedParametersProps={{items: dropDownPanelProps}}
+        description="DropDownPanel-description"
+        example={DropDownPanelExample}
+        name="DropDownPanel"
+        view={DropDownPanelView}
     />
 );
