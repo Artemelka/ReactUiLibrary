c9af936fec5ad36f117c1f474b84f940ea58bdb0
artemelka <artemelka.ru@gmail.com>
2019-07-28 14:34:27 +0300

    rewrite icon sandbox

---
+++ src/app/pages/sand-box/icon/components/icon-view.tsx
@@ -0,0 +1,59 @@
+import React, { Fragment } from 'react';
+import classNames from 'classnames/bind';
+import { IconModule } from '../../../../elements/icon';
+import { SandboxLayout } from '../../../../components';
+
+const { BlockItems } = SandboxLayout;
+const style = require('./icon-view.less');
+const cn = classNames.bind(style);
+const { Icon, IconNames } = IconModule;
+const iconProps = [
+    {
+        size: '2x',
+        pulse: true
+    }, {
+        stack: 'fas',
+        size: '2x',
+        fontSize: 16
+    }, {
+        stack: 'far',
+        inverse: true,
+        fontSize: 40
+    }, {
+        stack: 'fas',
+        inverse: true,
+        fontSize: 12
+    },
+];
+
+const renderTable = (index: number, name: string) => iconProps.map((props, propsIndex) => (
+    <tr className={cn('Table-icon-view__row')} key={index + propsIndex}>
+        <td className={cn('Table-icon-view__column')}>
+            <Icon name={name} {...props}/>
+        </td>
+        <td className={cn('Table-icon-view__column')}>{name.toUpperCase()}</td>
+        <td className={cn('Table-icon-view__column')}>{props.stack}</td>
+    </tr>
+));
+export const IconView = () => (
+    <Fragment>
+        {Object.values(IconNames).map((item, index) => (
+            <BlockItems key={index}>
+                <table className={cn('Table-icon-view')}>
+                    <tbody>
+                    <tr>
+                        <th className={cn('Table-icon-view__header-column')}>Icon</th>
+                        <th className={cn('Table-icon-view__header-column')}>name</th>
+                        <th className={cn('Table-icon-view__header-column')}>stack</th>
+                    </tr>
+                    {
+                        typeof item === 'string'
+                            ? renderTable(index, item)
+                            : Object.values(item).map((name, index) => renderTable(index, name))
+                    }
+                    </tbody>
+                </table>
+            </BlockItems>
+        ))}
+    </Fragment>
+);
--- src/app/pages/sand-box/icon/icon-sand-box.tsx
+++ src/app/pages/sand-box/icon/icon-sand-box.tsx
@@ -1,59 +1,27 @@
 import React from 'react';
-import classNames from 'classnames/bind';
-import { SandBox } from '../sand-box';
-import { IconModule } from '../../../elements/icon';
+import { SandboxLayout } from '../../../components';
+import { IconExample } from './components/icon-example';
+import { IconView } from './components/icon-view';
 
-const style = require('./icon-sand-box.less');
-const cn = classNames.bind(style);
-const { Icon, IconNames } = IconModule;
+const { Sandbox } = SandboxLayout;
 const iconProps = [
-    {
-        size: '2x',
-        pulse: true
-    }, {
-        stack: 'fas',
-        size: '2x',
-        fontSize: 16
-    }, {
-        stack: 'far',
-        inverse: true,
-        fontSize: 40
-    }, {
-        stack: 'fas',
-        inverse: true,
-        fontSize: 12
-    },
+    ['border', 'boolean', ''],
+    ['inverse', 'boolean', ''],
+    ['name', 'string', '', 'yes'],
+    ['pulse', 'boolean', ''],
+    ['size', 'string', ''],
+    ['spin', 'boolean', ''],
+    ['stack', 'string', ''],
+    ['fontSize', 'number', '']
 ];
 
-const renderTable = (index: number, name: string) => iconProps.map((props, propsIndex) => (
-        <tr key={index + propsIndex} style={{paddingBottom: '20px'}}>
-            <td className={cn('table-column')}>
-                <Icon name={name} {...props}/>
-            </td>
-            <td className={cn('table-column')}>{name.toUpperCase()}</td>
-            <td className={cn('table-column')}>{props.stack}</td>
-        </tr>
-));
-const iconItems = Object.values(IconNames).map((item, index) => () => (
-    <table  >
-        <tbody>
-        <tr>
-            <th className={cn('table-column')}>Icon</th>
-            <th className={cn('table-column')}>name</th>
-            <th className={cn('table-column')}>stack</th>
-        </tr>
-        {
-            typeof item === 'string'
-                ? renderTable(index, item)
-                : Object.values(item).map((name, index) => renderTable(index, name))
-        }
-        </tbody>
-    </table>
-));
-
 export const IconSandBox = () => (
-    <SandBox
-        items={iconItems}
+    <Sandbox
+        acceptedParametersProps={{items: iconProps}}
+        description="icon-description"
+        example={IconExample}
+        name="Icon"
+        view={IconView}
     />
 );
 
