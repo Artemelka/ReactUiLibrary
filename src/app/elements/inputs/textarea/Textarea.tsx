194ac34f84c48d298717e052fde0664244737196
Artem Emelyanov <anemelyanov@vtb.ru>
2019-08-05 19:16:49 +0300

    rewrite textarea

--- src/app/elements/inputs/textarea/Textarea.tsx
+++ src/app/elements/inputs/textarea/Textarea.tsx
@@ -1,127 +1,75 @@
-import React, {
-    Component,
-    ComponentType,
-    createRef,
-    KeyboardEvent,
-    MouseEvent,
-    RefObject,
-    SyntheticEvent
-} from 'react';
-import classNames from 'classnames/bind';
-import { IconModule } from '../../icon';
-import { Button } from '../../buttons/Button';
+import React, { Component, createRef, RefObject, SyntheticEvent } from 'react';
+import { TextareaUI, TextareaProps } from './Textarea-UI';
 
-const style = require('./Textarea.less');
-const cn = classNames.bind(style);
-const { IconNames: {BACKSPACE} } = IconModule;
+const TEXTAREA_MAX_LENGTH = 255;
+const ERROR_MESSAGE = 'max length';
+const errorParams = {
+    error: true,
+    errorMessage: ERROR_MESSAGE
+};
 
-export interface TextareaProps {
-    cols?: number;
-    darkTheme?: boolean;
-    disabled?: boolean;
-    error?: boolean;
-    errorMessage?: string;
-    id: string;
-    maxlength?: number;
-    name: string;
-    onBlur?: (event: SyntheticEvent<HTMLTextAreaElement>) => void;
-    onChange?: (event: SyntheticEvent<HTMLTextAreaElement>) => void;
-    onClearClick?: () => void;
-    onClick?: (event: MouseEvent<HTMLTextAreaElement> | KeyboardEvent) => void;
-    onFocus?: (event: SyntheticEvent<HTMLTextAreaElement>) => void;
-    onKeyDown?: (event: KeyboardEvent) => void;
-    onKeyPress?: (event: KeyboardEvent) => void;
-    onRef?: RefObject<HTMLTextAreaElement>;
-    placeholder?: string;
-    readonly?: boolean;
-    rows?: number;
-    translateComponent: ComponentType;
-    value: string;
-}
+type State = {
+    error: boolean,
+    errorMessage: string
+};
 
-export class Textarea extends Component<TextareaProps> {
+export class Textarea extends Component<TextareaProps, State> {
     static defaultProps = {
-        onClearClick: () => false,
-        rows: 8
+        maxlength: TEXTAREA_MAX_LENGTH
     };
 
+    state = {
+        error: false,
+        errorMessage: ''
+    };
+
+    setErrorState = (event: SyntheticEvent<HTMLTextAreaElement>, value: string) => {
+        this.setState(errorParams);
+        this.props.onChange(event, value.slice(0, this.props.maxlength));
+    };
+
+    changeState = (event: SyntheticEvent<HTMLTextAreaElement>, value: string) => {
+        this.setState((state) => ({error: state.error ? false : state.error}));
+        this.props.onChange(event, value);
+    };
+
+    handleChange = (event: SyntheticEvent<HTMLTextAreaElement>) => {
+        const { value } = event.currentTarget;
+        const { maxlength } = this.props;
+
+        if (value.length <= maxlength) {
+            this.changeState(event, value);
+        }
+
+        if (value.length > maxlength) {
+            this.setErrorState(event, value);
+        }
+    };
+
+    handleClearClick = () => {
+        this.ref.current.focus();
+        this.setState({error: false});
+        this.props.onChange(null, '');
+    };
+
+    ref: RefObject<HTMLTextAreaElement> = createRef();
+
     render() {
-        const {
-            cols,
-            disabled,
-            darkTheme,
-            error,
-            errorMessage,
-            id,
-            maxlength,
-            name,
-            onBlur,
-            onChange,
-            onClearClick,
-            onClick,
-            onFocus,
-            onKeyDown,
-            onKeyPress,
-            onRef,
-            placeholder,
-            readonly,
-            rows,
-            translateComponent: Translate,
-            value
-        } = this.props;
+        const { error, errorMessage, id, value, ...restProps } = this.props;
+        const { error: innerError, errorMessage: innerMessage } = this.state;
 
         return (
-            <div className={cn('Textarea', {'Textarea--disabled': disabled})}>
-                <textarea
-                    className={cn('Textarea__input', {
-                        'Textarea__input--dark': darkTheme,
-                        'Textarea__input--error': error,
-                        'Textarea__input--disabled': disabled
-                    })}
-                    cols={cols}
-                    disabled={disabled}
-                    id={id}
-                    // maxLength={maxlength}
-                    name={name}
-                    onBlur={onBlur}
-                    onChange={onChange}
-                    onClick={onClick}
-                    onFocus={onFocus}
-                    onKeyDown={onKeyDown}
-                    onKeyPress={onKeyPress}
-                    placeholder={placeholder}
-                    readOnly={readonly}
-                    ref={onRef}
-                    rows={rows}
-                    spellCheck
-                    value={value}
-                />
-                {Boolean(value.length) && !disabled &&
-                    <div className={cn('Textarea__clear-button')}>
-                        <Button.Icon
-                            iconName={BACKSPACE}
-                            onClick={onClearClick}
-                        />
-                    </div>
-                }
-                {error &&
-                    <div className={cn('Textarea__error-container')}>
-                        {errorMessage && <Translate translateKey={errorMessage}/>}
-                    </div>
-                }
-                <div className={cn('Textarea__counter-container')}>
-                    <span className={cn('Textarea__max-value')}>
-                        {`${maxlength} / `}
-                    </span>
-                    <span
-                        className={cn('Textarea__value-counter', {
-                            'Textarea__value-counter--error': error
-                        })}
-                    >
-                        {`${value.length}`}
-                    </span>
-                </div>
-            </div>
+            <TextareaUI
+                {...restProps}
+                error={error || innerError}
+                errorMessage={errorMessage || innerMessage}
+                id={id}
+                name={id}
+                onChange={this.handleChange}
+                onClearClick={this.handleClearClick}
+                onRef={this.ref}
+                value={value}
+            />
         );
     }
 }
--- src/app/pages/sand-box/taxtarea/components/textarea-container.tsx
+++ src/app/pages/sand-box/taxtarea/components/textarea-container.tsx
@@ -1,8 +1,6 @@
-import React, { Component, createRef, KeyboardEvent, RefObject, SyntheticEvent } from 'react';
+import React, { Component, createRef, RefObject, SyntheticEvent } from 'react';
 import { Textarea, TranslateComponent } from '../../../../elements';
-import { TextareaProps } from '../../../../elements/inputs/textarea/Textarea';
-
-export const TEXTAREA_MAX_LENGTH = 255;
+import { TextareaProps } from '../../../../elements/inputs/textarea/Textarea-UI';
 
 type State = {
     error: boolean,
@@ -11,79 +9,48 @@ type State = {
 };
 
 export class TextareaContainer extends Component<TextareaProps, State> {
-    static defaultProps = {
-        maxlength: TEXTAREA_MAX_LENGTH,
-        onBlur: () => false
-    };
-
     state = {
         error: false,
         errorMessage: '',
         value: ''
     };
 
-    setErrorState = (
-        value: string,
-        onChange: (value: string) => void
-    ) => {
-        const correctValue = value.slice(0, this.props.maxlength);
-
+    setErrorState = () => {
         this.setState({
             error: true,
-            errorMessage: 'max length',
-            value: correctValue
+            errorMessage: 'not have empty'
         });
-        onChange(correctValue);
     };
 
     changeState = (value: string) =>
         this.setState((state) => ({
             error: state.error ? false : state.error,
             value
-        })
-    );
+        }));
 
-    handleBlur = (event: SyntheticEvent<HTMLTextAreaElement>) => {
-        this.props.onBlur(event);
-    };
-
-    handleChange = (event: SyntheticEvent<HTMLTextAreaElement>) => {
-        const { value } = event.currentTarget;
-        const { maxlength, onChange } = this.props;
-
-        if (value.length <= maxlength) {
-            this.changeState(value);
-            onChange(value);
-        }
-
-        if (value.length > maxlength) {
-            this.setErrorState(value, onChange);
+    handleChange = (event: SyntheticEvent<HTMLTextAreaElement>, value: string) => {
+        this.changeState(value);
+        if (!value.length) {
+            console.log('error');
+            this.setErrorState();
+        } else {
+            // this.props.onChange(event, value);
+            console.log('onChange');
         }
     };
-
-    handleClearClick = () => {
-        this.ref.current.focus();
-        this.setState({value: ''});
-    };
-
-    ref: RefObject<HTMLTextAreaElement> = createRef();
 
     render() {
-        const { error, errorMessage, id, ...restProps } = this.props;
-        const { error: innerError, errorMessage: innerMessage, value } = this.state;
+        const { id, ...restProps } = this.props;
+        const { error, errorMessage, value } = this.state;
 
         return (
             <Textarea
                 {...restProps}
-                error={error || innerError}
-                errorMessage={errorMessage || innerMessage}
+                error={error}
+                errorMessage={errorMessage}
                 id={id}
-                maxlength={TEXTAREA_MAX_LENGTH}
                 name={id}
-                onBlur={this.handleBlur}
                 onChange={this.handleChange}
-                onClearClick={this.handleClearClick}
-                onRef={this.ref}
                 translateComponent={TranslateComponent}
                 value={value}
             />
