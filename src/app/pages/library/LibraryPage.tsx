97417c4f244540fc7294e420747fa587ed9182f2
Artem Emelyanov <anemelyanov@vtb.ru>
2019-08-26 17:43:39 +0300

    fix library page

--- src/app/pages/library/LibraryPage.tsx
+++ src/app/pages/library/LibraryPage.tsx
@@ -4,23 +4,16 @@ import { SideBar } from '../../components';
 import { SandBoxPage } from '../index';
 import { ComponentsCollection } from '../constants';
 import { LibraryFooter } from './LibraryFooter';
-import { SideBarLink } from '../../components/side-bar/SideBarLink';
 
 const { Aside, Footer, Header, Main, Page } = PageLayout;
-const [ menuItem, ...restItems ] = ComponentsCollection;
 const ASIDE_HEADING = 'React UI Library';
 const ASIDE_TEXT = '123456789';
-
-type State = {
-    withIndex: boolean;
-};
 
 export const LibraryPage = () => (
     <Page>
         {/*<Header />*/}
         <Aside heading={ASIDE_HEADING} footerText={ASIDE_TEXT} withoutHeader>
-            <SideBarLink name={menuItem.name} url={menuItem.url}/>
-            <SideBar items={restItems}/>
+            <SideBar items={ComponentsCollection}/>
         </Aside>
         <Main withoutHeader>
             <SandBoxPage />
