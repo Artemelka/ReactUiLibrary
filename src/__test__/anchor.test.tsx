cc8eecd8b5176ee54caf0c0d643438f299a5f3f5
Artem Emelyanov <anemelyanov@vtb.ru>
2019-08-27 14:19:18 +0300

    fix anchor component tests

--- src/__test__/anchor.test.tsx
+++ src/__test__/anchor.test.tsx
@@ -4,20 +4,20 @@ import { Anchor } from '../app/elements';
 
 describe('Anchor test', () => {
     test('Render snapshot anchor', () => {
-        const output = shallow(<Anchor href="test" label="test"/>);
+        const output = shallow(<Anchor href="test">test</Anchor>);
 
         expect(output).toMatchSnapshot();
     });
 
     test('Render snapshot pseudoLink', () => {
-        const output = shallow(<Anchor label="test"/>);
+        const output = shallow(<Anchor>test</Anchor>);
 
         expect(output).toMatchSnapshot();
     });
 
     test('Expect call onClick', () => {
         const testMock = jest.fn();
-        const output = mount(<Anchor label="test" onClick={testMock}/>);
+        const output = mount(<Anchor onClick={testMock}>test</Anchor>);
 
         output.simulate('click');
 
@@ -25,35 +25,35 @@ describe('Anchor test', () => {
     });
 
     test('Expect style active with prop active', () => {
-        const output = mount(<Anchor label="test" active/>);
+        const output = mount(<Anchor active>test</Anchor>);
 
 
         expect(output.find('.Anchor').hasClass('Anchor--active')).toEqual(true);
     });
 
     test('Expect style disabled with prop disabled', () => {
-        const output = mount(<Anchor label="test" disabled/>);
+        const output = mount(<Anchor disabled>test</Anchor>);
 
 
         expect(output.find('.Anchor').hasClass('Anchor--disabled')).toEqual(true);
     });
 
     test('Expect attr target with prop newPage', () => {
-        const output = mount(<Anchor href="test" label="test" newPage/>);
+        const output = mount(<Anchor href="test" newPage>test</Anchor>);
 
 
         expect(output.find('.Anchor').props().target).toEqual('_blank');
     });
 
     test('Expect tag name "a" with prop href', () => {
-        const output = mount(<Anchor href="test" label="test" />);
+        const output = mount(<Anchor href="test">test</Anchor>);
 
 
         expect(output.find('.Anchor').type()).toEqual('a');
     });
 
     test('Expect tag name "span" without prop href', () => {
-        const output = mount(<Anchor label="test" />);
+        const output = mount(<Anchor >test</Anchor>);
 
 
         expect(output.find('.Anchor').type()).toEqual('span');
