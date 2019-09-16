e4a439be9d974cebf0c4db61502a7dc54f91da55
artemelka <artemelka.ru@gmail.com>
2019-09-17 02:37:12 +0300

    add mongoose schema

--- src/server/routes/translate/index.js
+++ src/server/routes/translate/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { DictionaryKey, RuLocale } from '../../db';
 import dictionary from './dictionary.json';
 
 export const translate = express.Router();
 
 translate.get('/', (request, response) => {
+
+    DictionaryKey.find({}, null, null, (error, data) => console.log('======= response DictionaryKey =========', data));
+    RuLocale.find({}, null, null, (error, data) => console.log('======= response RuLocale =========', data));
+
     return response.json(dictionary);
 });
 
