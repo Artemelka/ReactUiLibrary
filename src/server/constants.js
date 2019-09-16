e270776d6191d6485e934c07f2e1d305d07075ca
artemelka <artemelka.ru@gmail.com>
2019-09-17 02:37:12 +0300

    add mongoose schema

--- src/server/constants.js
+++ src/server/constants.js
@@ -11,3 +11,14 @@ export const ProxyParams = {
     SERVER: `http://localhost:${ServerParams.PORT}`
 };
 
+export const DbUserParams = {
+    USER: 'reactKitAdmin',
+    PASSWORD: '61406140'
+};
+const DB_URL_PARAMS = 'retryWrites=true&w=majority';
+const DB_HOST = '@cluster0-ahlbz.gcp.mongodb.net';
+const DB_NAME = 'language';
+// const DB_NAME = 'test';
+
+export const DB_URL = `mongodb+srv://${DbUserParams.USER}:${DbUserParams.PASSWORD}${DB_HOST}/${DB_NAME}?${DB_URL_PARAMS}`;
+export const DB_URL_CUT = `mongodb://${DbUserParams.USER}:${DbUserParams.PASSWORD}${DB_HOST}/${DB_NAME}`;
