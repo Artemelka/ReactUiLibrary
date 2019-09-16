642022ce14af9871a9ba7d2635ef40bd5de6e127
artemelka <artemelka.ru@gmail.com>
2019-09-17 02:37:12 +0300

    add mongoose schema

--- src/server/app.js
+++ src/server/app.js
@@ -2,7 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import fileUpload from 'express-fileupload';
 import reload from 'express-reload';
-import { ServerParams, KEEP_ALIVE_TIMEOUT } from './constants';
+import mongoose from 'mongoose';
+import { DB_URL, DB_URL_CUT, ServerParams, KEEP_ALIVE_TIMEOUT } from './constants';
 
 const app = express();
 const { PORT, HOST } = ServerParams;
@@ -14,9 +15,16 @@ const listenCallback = error => error
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(fileUpload());
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 app.use('/api', reload(path));
 
 const appServer = app.listen(PORT, HOST, listenCallback);
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.error(error));
 
 appServer.keepAliveTimeout = KEEP_ALIVE_TIMEOUT;
