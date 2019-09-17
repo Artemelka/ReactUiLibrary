7d45d2f5b8b5b6b8d47e2b869afa0dea8e057642
Artem Emelyanov <anemelyanov@vtb.ru>
2019-09-17 21:24:10 +0300

    fix mongoose db model

--- src/server/app.js
+++ src/server/app.js
@@ -3,14 +3,16 @@ import bodyParser from 'body-parser';
 import fileUpload from 'express-fileupload';
 import reload from 'express-reload';
 import mongoose from 'mongoose';
-import { DB_URL, DB_URL_CUT, ServerParams, KEEP_ALIVE_TIMEOUT } from './constants';
+import { DbConnectParams, DB_URI, ServerParams, KEEP_ALIVE_TIMEOUT } from './constants';
 
 const app = express();
 const { PORT, HOST } = ServerParams;
 const path = __dirname + '/routes/';
 const listenCallback = error => error
-    ? console.log('something bad happened', error)
-    : console.log(`Listening on port ${PORT}!`);
+    ? console.error('Server not started! ERROR:', error)
+    : console.log(`Server started on port ${PORT}!`);
+const handleDbConnection = () => console.log('Connected to MongoDB!');
+const handleDbConnectionError = error => console.error('MongoDB connected error', error)
 
 app.use(bodyParser.json());
 app.use(bodyParser.text());
@@ -19,12 +21,10 @@ app.use(express.static('dist'));
 app.use('/api', reload(path));
 
 const appServer = app.listen(PORT, HOST, listenCallback);
-
-mongoose.connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Connected to DB!'))
-    .catch(error => console.error(error));
-
 appServer.keepAliveTimeout = KEEP_ALIVE_TIMEOUT;
+
+mongoose.connect(DB_URI, DbConnectParams)
+    .then(handleDbConnection)
+    .catch(handleDbConnectionError);
+
+
