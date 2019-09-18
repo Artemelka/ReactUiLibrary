16f1724ae49b9a1d770e9ff1da9e702c642d2a35
Artem Emelyanov <anemelyanov@vtb.ru>
2019-09-18 20:10:09 +0300

    write controller addDictionary

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,16 +1,14 @@
 import mongoose, { Schema } from 'mongoose';
 
 const { ObjectId } = Schema;
-const translateDictionaryData = {
+const COLLECTION_NAME = 'translate_dictionary';
+const translateDictionarySchema = new Schema({
     id: ObjectId,
     locale: String,
     enabled: Boolean,
     dictionary: Object
-};
-
-const translateDictionarySchema = new Schema(translateDictionaryData);
-
-const TranslateDictionary = mongoose.model('translate_dictionary', translateDictionarySchema, 'translate_dictionary');
+});
+const TranslateDictionary = mongoose.model(COLLECTION_NAME, translateDictionarySchema, COLLECTION_NAME);
 
 const formatDictionary = (data) => {
     return data.reduce((result, item) => {
@@ -22,19 +20,34 @@ const formatDictionary = (data) => {
     }, {})
 };
 
-export const getDictionary = () => {
-    return new Promise((resolve, reject) => {
-        TranslateDictionary.find({}).exec((error, data) => {
+export const getDictionary = () => new Promise((resolve, reject) =>
+    TranslateDictionary.find({})
+        .exec((error, data) =>
+            error
+                ? reject(error)
+                : resolve(formatDictionary(data)))
+);
+
+export const addDictionary = (arrData) => new Promise((resolve, reject) => {
+    arrData.map(collectionItem => {
+        const localeDictionary = new TranslateDictionary({
+            locale: collectionItem.locale,
+            enabled: true,
+            dictionary: collectionItem.dictionary
+        });
+
+        localeDictionary.save(error => {
             if (error) {
-                console.log('======= error DictionaryKey =========', error);
-                reject(error);
-            } else {
-                resolve(formatDictionary(data));
+                return console.log('save error', error);
             }
+
+            console.log('saved');
         });
     });
 
-};
+    getDictionary().then(value => resolve(value)).catch(error => reject(error))
+
+});
 
 
 
