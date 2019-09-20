9f03146f62c8ce4d7e0d44b08b74b38e77466265
artemelka <artemelka.ru@gmail.com>
2019-09-21 01:36:06 +0300

    add dictionary key controller

---
+++ src/server/controllers/translate-dictionary/index.js
@@ -0,0 +1,3 @@
+export { addDictionaryController } from './add-dictionary-controller';
+export { getDictionaryController } from './get-dictionary-controller';
+export { addDictionaryKeyController } from './add-dictionary-key-controller';
