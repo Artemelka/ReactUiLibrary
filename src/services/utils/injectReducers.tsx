62d9f58383548ebc5d46e8813a23ef7c7a470d5a
artemelka <artemelka.ru@gmail.com>
2020-01-08 01:27:58 +0300

    fiX

--- src/services/utils/injectReducers.tsx
+++ src/services/utils/injectReducers.tsx
@@ -1,68 +1,6 @@
 import React, { Component, ComponentType, FC } from 'react';
 import { Reducer, Store, StoreEnhancer, StoreCreator } from 'redux';
 
-// type DeepPartial<T> = {
-//     [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K]
-// };
-//
-// type Action<T = any> = {
-//     type: T
-// };
-//
-// type AnyAction = Action & {
-//     [extraProps: string]: any
-// };
-//
-// type Dispatch<A extends Action = AnyAction> = <T extends A>(action: T) => T;
-//
-// type Unsubscribe = () => void;
-//
-// type Observer<T> = {
-//     next?(value: T): void
-// };
-//
-// type Observable<T> = {
-//     subscribe: (observer: Observer<T>) => { unsubscribe: Unsubscribe }
-//     [Symbol.observable](): Observable<T>
-// };
-//
-// type Store<S = any, A extends Action = AnyAction> = {
-//     dispatch: Dispatch<A>;
-//     getState(): S;
-//     subscribe(listener: () => void): Unsubscribe;
-//     replaceReducer(nextReducer: Reducer<S, A>): void;
-//     [Symbol.observable](): Observable<S>;
-// };
-//
-// type Reducer<S = any, A extends Action = AnyAction> = (
-//     state: S | undefined,
-//     action: A
-// ) => S;
-//
-// type StoreEnhancerStoreCreator<Ext = {}, StateExt = {}> = <
-//     S = any,
-//     A extends Action = AnyAction
-//     >(
-//     reducer: Reducer<S, A>,
-//     preloadedState?: DeepPartial<S>
-// ) => Store<S & StateExt, A> & Ext;
-//
-// type StoreEnhancer<Ext = {}, StateExt = {}> = (
-//     next: StoreEnhancerStoreCreator
-// ) => StoreEnhancerStoreCreator<Ext, StateExt>;
-//
-// type StoreCreator = {
-//     <S, A extends Action, Ext, StateExt>(
-//         reducer: Reducer<S, A>,
-//         enhancer?: StoreEnhancer<Ext, StateExt>
-//     ): Store<S & StateExt, A> & Ext
-//     <S, A extends Action, Ext, StateExt>(
-//         reducer: Reducer<S, A>,
-//         preloadedState?: DeepPartial<S>,
-//         enhancer?: StoreEnhancer<Ext>
-//     ): Store<S & StateExt, A> & Ext
-// };
-
 type AsyncReducerMap = Record<string, Reducer>;
 type CreateReducer = (asyncReducers?: AsyncReducerMap) => Reducer;
 type AsyncReducerItem = {
