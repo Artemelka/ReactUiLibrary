07deffe68298d292caf44752fbb230ac37c9aa6a
artemelka <artemelka.ru@gmail.com>
2020-01-08 01:47:12 +0300

    resolve conflict

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,3 @@
 export { mapPropsToClasses } from './map-props-to-classes/map-props-to-classes';
 export { nameSpaceCreator } from './name-spase-creator';
-export { createStoreWithInsertReducer } from './utils/injectReducers';
+export { createStoreWithInsertReducer } from './inject-reducer/inject-reducers';
