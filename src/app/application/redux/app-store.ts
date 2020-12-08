0afac91da55fe66ef1079e79c67c0d65084acc62
Artemelka <emelyanov.artem@wildberries.ru>
2020-12-08 06:54:29 +0300

    remove temp func

--- src/app/application/redux/app-store.ts
+++ src/app/application/redux/app-store.ts
@@ -1,7 +1,6 @@
-import { Middleware, Reducer } from 'redux';
-import { Saga } from 'redux-saga';
+import { Middleware } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
-import { injectAsyncSaga, injectAsyncReducer, createAppStore, removeAsyncSaga, removeAsyncReducer } from '@wildberries/redux-core-modules';
+import { createAppStore } from '@wildberries/redux-core-modules';
 import { ROOT_REDUCERS } from './reducer';
 import logger from 'redux-logger'; // tslint:disable-line:no-implicit-dependencies
 import { history } from './app-history';
@@ -15,23 +14,3 @@ if (process.env && process.env.MODE === 'development') {
 }
 
 export const appStore = createAppStore({ extraMiddlewares: middlewares, rootReducers: ROOT_REDUCERS });
-
-export type InjectReducerParamsType = {
-    name: string;
-    reducer: Reducer;
-};
-export type InjectSagaParamsType = {
-    name: string;
-    saga: Saga;
-};
-
-export const injectReducer = ({ name, reducer }: InjectReducerParamsType) =>
-    injectAsyncReducer({ store: appStore, name, reducer });
-
-export const injectSaga = ({ name, saga }: InjectSagaParamsType) => injectAsyncSaga({ store: appStore, name, saga });
-
-export const removeReducer = ({ name }: InjectReducerParamsType) => removeAsyncReducer({ store: appStore, name });
-
-export const removeSaga = ({ name }: InjectSagaParamsType) => removeAsyncSaga({ store: appStore, name });
-
-
